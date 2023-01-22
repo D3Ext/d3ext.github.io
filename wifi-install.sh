828d00db13450015982c0c0023d98d936abee7ac
D3Ext <67125397+D3Ext@users.noreply.github.com>
2023-01-22 21:27:37 +0100

    Update wifi-install.sh

--- wifi-install.sh
+++ wifi-install.sh
@@ -14,13 +14,8 @@ if [ "$UID" != "0" ]; then
   exit 0
 fi
 
-if [ ! -f "/etc/debian_version" ]; then
-  echo -e "${blueColour}El sistema actual no esta basado en Debian, ejecuta este script en: Ubuntu, Parrot, Kali Linux...${endColour}"
-  exit 1
-fi
-
 # Requerimientos
-reqs=(git wget curl python3 python3-pip aircrack-ng mdk4 reaver pixiewps hostapd xterm hashcat dnsmasq hcxtools iptables build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev wpasupplicant tcpdump screen iw usbutils wireshark tshark python3-scapy python3-pyqt5 python3-numpy python3-scipy)
+reqs=(git wget curl python3 python3-pip aircrack-ng mdk4 reaver pixiewps hostapd xterm hashcat dnsmasq moreutils hcxtools iptables build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev wpasupplicant tcpdump screen iw usbutils wireshark tshark python3-scapy python3-pyqt5 python3-numpy python3-scipy)
 
 # Instala los requerimientos
 clear; echo -e "${yellowColour} _       ___ _____       ____           __        ____
@@ -30,12 +25,27 @@ clear; echo -e "${yellowColour} _       ___ _____       ____           __       
 |__/|__/_/_/ /_/     /___/_/ /_/____/\__/\__,_/_/_/${endColour}"
 
 sleep 0.2
+if [ -f "/etc/debian_version" ]; then
+  echo -e "${blueColour}Sistema detectado: Debian based${endColour}"
+elif [ -f "/etc/arch-release" ]; then
+  echo -e "${blueColour}Sistema detectado: Arch based${endColour}"
+else
+  echo -e "${blueColour}El sistema no es ni Debian based ni Arch based!${endColour}"
+  exit 0
+fi
+sleep 0.2
 echo -e "${blueColour}\nInstalando paquetes requeridos\n${endColour}"
 sleep 0.4
 
-for req in ${reqs[@]}; do
-  apt install ${req} -y 2>/dev/null
-done
+if [ -f "/etc/debian_version" ]; then
+  for req in ${reqs[@]}; do
+    apt install ${req} -y 2>/dev/null
+  done
+elif [ -f "/etc/arch-release" ]; then
+  for req in ${reqs[@]}; do
+    pacman -S ${req} --noconfirm 2>/dev/null
+  done
+fi
 
 wget https://raw.githubusercontent.com/hashcat/hashcat-utils/master/src/cap2hccapx.c 2>/dev/null
 gcc cap2hccapx.c -o /usr/bin/cap2hccapx 2>/dev/null
@@ -45,19 +55,14 @@ sleep 0.2
 echo -e "${blueColour}\nRequerimientos instalados correctamente${endColour}"
 sleep 0.3
 
-# Clona drivers y WEF
-echo -e "${blueColour}\nClonando drivers, visita la documentacion oficial para terminar de instalarlos en caso de que tu antena no sea reconocida${endColour}"
-sleep 0.3
-
-git clone https://github.com/aircrack-ng/rtl8188eus
-git clone https://github.com/aircrack-ng/rtl8812au
-
+# Descarga WEF y qspectrumanalyzer
 echo -e "${blueColour}\nInstalando qspectrumanalyzer${endColour}"
 
 sleep 0.3
 git clone https://github.com/xmikos/qspectrumanalyzer.git
 cd qspectrumanalyzer 2>/dev/null
 pip3 install --user . 2>/dev/null
+cd ..
 sleep 0.3
 
 echo -e "${blueColour}\nDescargando WEF${endColour}"
